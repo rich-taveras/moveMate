d30d1737135bf0965b183c2dff602bd5909e6c56
Julio Bermudez <julioph0n3@gmail.com>
2023-10-02 02:10:15 -0400

    Finish 98% of frontend fancy, need review and find bugs

--- public/js/home.js
+++ public/js/home.js
@@ -1,22 +1,30 @@
-function movement() {
-
-    for (let i = 0; i < 13; i++) {        
-        let numEl = document.getElementById(`num${i}`) ;
-        let value = numEl.getAttribute("data-count");
-      let iniValue = 0;
-      let numSpeed = 10;
-      let count = () => {
-        iniValue = iniValue + numSpeed;
-            
-        numEl.innerHTML = "+" + iniValue
-        if (iniValue < value) {
-          setTimeout(() => {
-            count();
-          }, 30);
-        }
-      };
-      count();
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      movement();
     }
-  }
+  });
+});
+observer.observe(document.getElementById('custom-number'));
 
-  movement();
+function movement() {
+  for (let i = 0; i < 13; i++) {
+    let numEl = document.getElementById(`num${i}`);
+    let value = numEl.getAttribute('data-count');
+    let iniValue = 0;
+    let numSpeed = 10;
+    let count = () => {
+      iniValue = iniValue + numSpeed;
+
+      numEl.innerHTML = '+' + iniValue;
+      if (iniValue < value) {
+        setTimeout(() => {
+          count();
+        }, 30);
+      }
+    };
+    count();
+  }
+}
+
+movement();
