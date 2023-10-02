8a6c7a2ddc2deaaf74fd2fff9f53f6d0e1f6aaff
Julio Bermudez <julioph0n3@gmail.com>
2023-10-02 18:14:50 -0400

    deleted all files extra

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
-const testRoutes= require('./testRoute');
+
 
 
 //modular html routes
--- models/index.js
+++ models/index.js
@@ -1,13 +1,7 @@
 const User = require('./User');
-const Project = require('./Project');
 
-User.hasMany(Project, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
-});
 
-Project.belongsTo(User, {
-  foreignKey: 'user_id'
-});
 
-module.exports = { User, Project };
+User.belongsTo(User)
+
+module.exports = { User };
